9cbe177a5b26ef9c7327d53bdf42b42e37e5158f
Igor Ityanagui de Andrade <igorityanagui@hotmail.com>
2023-08-21 19:34:42 -0300

    ACABOU QUASE

--- Assets/Scripts/EnemyHealth.cs
+++ Assets/Scripts/EnemyHealth.cs
@@ -17,13 +17,33 @@ public class EnemyHealth : MonoBehaviour
     }
 
     // Update is called once per frame
-    void Update()
+    public void TakeDamage(int amount)
     {
-        //healthBar.fillAmount = Mathf.Clamp(health / maxHealth, 0, 1);
+        health -= amount; // Reduz a vida atual pelo valor do dano.
 
         if (health <= 0)
         {
-            Destroy(gameObject);
+            Die(); // Chama uma função para lidar com a morte do inimigo.
+        }
+    }
+
+    void Die()
+    {
+        // Lógica para a morte do inimigo, como tocar uma animação, reproduzir som, etc.
+        Destroy(gameObject); // Destroi o objeto do inimigo.
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("PlayerProjectile")) // Verifica a tag do objeto colidido.
+        {
+            // Aplica dano ao inimigo quando o projétil do jogador colidir.
+            PlayerProjectile playerProjectile = collision.gameObject.GetComponent<PlayerProjectile>();
+            if (playerProjectile != null)
+            {
+                TakeDamage(playerProjectile.damageAmount); // Aplica dano ao inimigo.
+                Destroy(collision.gameObject); // Destroi o projétil do jogador.
+            }
         }
     }
 }
---
+++ Assets/Scripts/PlayerProjectile.cs
@@ -0,0 +1,8 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerProjectile : MonoBehaviour
+{
+    public int damageAmount = 20; // Quantidade de dano causada pelo projétil.
+}
